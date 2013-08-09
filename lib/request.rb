ec8ce7459beada2356e07dea5fcf4092b8e044ee
Nathan Woodhull <woodhull@gmail.com>
2013-08-09 15:39:35 -0400

    move things around.

--- lib/request.rb
+++ lib/request.rb
@@ -24,8 +24,26 @@ module Vertebrae
       request(:delete, path, params, options)
     end
 
-    def request(method, path, params, options)
-      raise 'implement me'
+    def request(method, path, params, options) # :nodoc:
+      if !::Vertebrae::Request::METHODS.include?(method)
+        raise ArgumentError, "unknown http method: #{method}"
+      end
+
+      path =  connection.configuration.prefix + '/' + path
+
+      ::Vertebrae::Base.logger.debug "EXECUTED: #{method} - #{path} with #{params} and #{options}"
+
+      connection.connection.send(method) do |request|
+
+        case method.to_sym
+          when *(::Vertebrae::Request::METHODS - ::Vertebrae::Request::METHODS_WITH_BODIES)
+            request.body = params.delete('data') if params.has_key?('data')
+            request.url(path, params)
+          when *::Vertebrae::Request::METHODS_WITH_BODIES
+            request.path = path
+            request.body = extract_data_from_params(params) unless params.empty?
+        end
+      end
     end
   end
 end
