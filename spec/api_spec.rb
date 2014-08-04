14561121e0224acd225db670f47af06eef0e30ec
Nathan Woodhull <woodhull@gmail.com>
2014-08-03 20:28:07 -0400

    better spec for initialization behavior that @dylandrop fixed.

--- spec/api_spec.rb
+++ spec/api_spec.rb
@@ -1,10 +1,25 @@
 require 'spec_helper'
 
 describe Vertebrae::API do
-
   subject { described_class.new(options) }
 
   it { described_class.included_modules.should include Vertebrae::Request }
+
+  describe 'initialize' do
+    before(:each) do
+      Vertebrae::API.any_instance.stub(:default_options).and_return({content_type: 'foo'})
+    end
+
+    context 'with an empty hash' do
+      let(:options) { {} }
+      specify { expect(subject.connection.options).to eq({content_type: 'foo'}) }
+    end
+
+    context 'with a default content-type' do
+      let(:options) { {content_type: 'text/csv'} }
+      specify { expect(subject.connection.options).to eq({content_type: 'text/csv'}) }
+    end
+  end
 
   describe 'dummy' do
     describe 'should delegate to the client class' do
